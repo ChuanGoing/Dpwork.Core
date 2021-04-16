193d3708f6ca3845e03f4971105e78e63c67939c
cbcao <cbcao@betawm.com>
2021-04-16 09:32:24 +0800

    json反序列化支持设置默认值

--- Dpwork.Core/Utils/SerializeUtil.cs
+++ Dpwork.Core/Utils/SerializeUtil.cs
@@ -19,15 +19,16 @@ namespace Dpwork.Core.Utils
             //Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
         };
 
-        public static string Serialize(object obj, JsonSerializerOptions options = null)
+        public static string Serialize(object obj, string defaultValue = "", JsonSerializerOptions options = null)
         {
-            if (obj == null) return string.Empty;
+            if (obj == null) return defaultValue;
 
             return JsonSerializer.Serialize(obj, obj.GetType(), options ?? Options);
         }
 
-        public static async Task<string> SerializeAsync(object value, JsonSerializerOptions options = null)
+        public static async Task<string> SerializeAsync(object value, string defaultValue = "", JsonSerializerOptions options = null)
         {
+            if (value == null) return defaultValue;
             using MemoryStream ms = new MemoryStream();
 
             await JsonSerializer.SerializeAsync(ms, value.GetType(), options ?? Options);
@@ -35,18 +36,21 @@ namespace Dpwork.Core.Utils
             return Encoding.UTF8.GetString(ms.ToArray());
         }
 
-        public static T Deserialize<T>(string json, JsonSerializerOptions options = null)
+        public static T Deserialize<T>(string json, T defaultValue = default(T), JsonSerializerOptions options = null)
         {
+            if (string.IsNullOrEmpty(json)) return defaultValue;
             return JsonSerializer.Deserialize<T>(json, options ?? Options);
         }
 
         public static object Deserialize(string json, Type returnType, JsonSerializerOptions options = null)
         {
+            if (string.IsNullOrEmpty(json)) return default;
             return JsonSerializer.Deserialize(json, returnType, options ?? Options);
         }
 
-        public static async Task<T> DeserializeAsync<T>(string json, JsonSerializerOptions options = null)
+        public static async Task<T> DeserializeAsync<T>(string json, T defaultValue = default(T), JsonSerializerOptions options = null)
         {
+            if (string.IsNullOrEmpty(json)) return defaultValue;
             using MemoryStream ms = new MemoryStream();
             var bytes = Encoding.UTF8.GetBytes(json);
             await ms.WriteAsync(bytes);
@@ -56,6 +60,7 @@ namespace Dpwork.Core.Utils
 
         public static IEnumerable<T> JsonToArry<T>(string json)
         {
+            if (string.IsNullOrEmpty(json)) return default;
             DataContractJsonSerializer _Json = new DataContractJsonSerializer(typeof(IEnumerable<T>));
             byte[] _Using = Encoding.UTF8.GetBytes(json);
             using MemoryStream _MemoryStream = new MemoryStream(_Using)
